41528a2f38ee4c5932a35c98b3a28b2070da9b36
Parth Patel <69315264+Parth511@users.noreply.github.com>
2022-05-04 15:33:40 +0530

    Update CoinFlip.sol

--- CoinFlip.sol
+++ CoinFlip.sol
@@ -1,36 +1,146 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;   //we are using antoher contract to crack into this contract
+/**
+ * CoinFlip
+ * 
+ * Juego de casino para apostar a cara o cruz. Se usa el timestamp del bloque de la blockchain
+ * para simular un número random entre 0 y 1. Si el timestamp del bloque es un número par el
+ * usuario gana el 190% del dinero invertido, si es un número impar pierde los ethers jugados.
+ *
+ * Al desplegar el contrato es necesario transferirle fondos para que pueda pagar el premio si
+ * el usuario gana.
+ *
+ * Versión modificada para interfaz web
+ * 
+**/
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+pragma solidity ^0.4.0;
 
 contract CoinFlip {
+    address owner;
+    uint payPercentage = 90;
+	
+	// Maximum amount to bet in WEIs
+	uint public MaxAmountToBet = 200000000000000000; // = 0.2 Ether
+	
+	struct Game {
+		address addr;
+		uint blocknumber;
+		uint blocktimestamp;
+        uint bet;
+		uint prize;
+        bool winner;
+    }
+	
+	Game[] lastPlayedGames;
+	
+	Game newGame;
+    
+    event Status(
+		string _msg, 
+		address user, 
+		uint amount,
+		bool winner
+	);
+    
+    function CoinFlip() payable {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner() {
+        if (owner != msg.sender) {
+            revert();
+        } else {
+            _;
+        }
+    }
+    
+    function Play() payable {
+		
+		if (msg.value > MaxAmountToBet) {
+			revert();
+		} else {
+			if ((block.timestamp % 2) == 0) {
+				
+				if (this.balance < (msg.value * ((100 + payPercentage) / 100))) {
+					// No tenemos suficientes fondos para pagar el premio, así que transferimos todo lo que tenemos
+					msg.sender.transfer(this.balance);
+					Status('Congratulations, you win! Sorry, we didn\'t have enought money, we will deposit everything we have!', msg.sender, msg.value, true);
+					
+					newGame = Game({
+						addr: msg.sender,
+						blocknumber: block.number,
+						blocktimestamp: block.timestamp,
+						bet: msg.value,
+						prize: this.balance,
+						winner: true
+					});
+					lastPlayedGames.push(newGame);
+					
+				} else {
+					uint _prize = msg.value * (100 + payPercentage) / 100;
+					Status('Congratulations, you win!', msg.sender, _prize, true);
+					msg.sender.transfer(_prize);
+					
+					newGame = Game({
+						addr: msg.sender,
+						blocknumber: block.number,
+						blocktimestamp: block.timestamp,
+						bet: msg.value,
+						prize: _prize,
+						winner: true
+					});
+					lastPlayedGames.push(newGame);
+					
+				}
+			} else {
+				Status('Sorry, you loose!', msg.sender, msg.value, false);
+				
+				newGame = Game({
+					addr: msg.sender,
+					blocknumber: block.number,
+					blocktimestamp: block.timestamp,
+					bet: msg.value,
+					prize: 0,
+					winner: false
+				});
+				lastPlayedGames.push(newGame);
+				
+			}
+		}
+    }
+	
+	function getGameCount() public constant returns(uint) {
+		return lastPlayedGames.length;
+	}
 
-  using SafeMath for uint256;  //sate variables
-  uint256 public consecutiveWins;
-  uint256 lastHash;
-  uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968; //this is what is used for guessing a random number
-
-  constructor() public {
-    consecutiveWins = 0;                    
-  }
-
-  function flip(bool _guess) public returns (bool) {       // notice that there is only one function. _guess is the value that will hold true or false will will judge the consectuive wins
-           uint256 blockValue = uint256(blockhash(block.number.sub(1))); //every transaction the blockhash. global func and global vars here. blockhash func takes input of block number so u can get the hash number. block.number is current blo. you CANNOT get the hash of the current block that is being mined, so therefore we subrtract one in order to work with previous block number. mining takes time. this is how they generate their numbers for their algorithm. each coinflip is a transaction. using the same blockhas is what gives u the consistent consecutive wins.   
-        
-            if (lastHash == blockValue) {                 //lastHash declared as state var
-              revert();                                  //this is what keeps us from hacking. it reverts any state changes
-              }    
-                    //if above not true, continue next block of code
-            lastHash = blockValue;
-            uint256 coinFlip = blockValue.div(FACTOR);
-            bool side = coinFlip == 1 ? true : false;
-        
-            if (side == _guess) {
-              consecutiveWins++;
-              return true;       //add one if true
-              } else {
-              consecutiveWins = 0;     //set back to 0
-              return false;
-         }
-      }
-   }
+	function getGameEntry(uint index) public constant returns(address addr, uint blocknumber, uint blocktimestamp, uint bet, uint prize, bool winner) {
+		return (lastPlayedGames[index].addr, lastPlayedGames[index].blocknumber, lastPlayedGames[index].blocktimestamp, lastPlayedGames[index].bet, lastPlayedGames[index].prize, lastPlayedGames[index].winner);
+	}
+	
+	
+	function depositFunds(uint amount) onlyOwner payable {
+        if (owner.send(amount)) {
+            Status('User has deposit some money!', msg.sender, msg.value, true);
+        }
+    }
+    
+	function withdrawFunds(uint amount) onlyOwner {
+        if (owner.send(amount)) {
+            Status('User withdraw some money!', msg.sender, amount, true);
+        }
+    }
+	
+	function setMaxAmountToBet(uint amount) onlyOwner returns (uint) {
+		MaxAmountToBet = amount;
+        return MaxAmountToBet;
+    }
+	
+	function getMaxAmountToBet(uint amount) constant returns (uint) {
+        return MaxAmountToBet;
+    }
+	
+    
+    function Kill() onlyOwner {
+        Status('Contract was killed, contract balance will be send to the owner!', msg.sender, this.balance, true);
+        selfdestruct(owner);
+    }
+}
